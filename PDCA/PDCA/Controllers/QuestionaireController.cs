bb2f349b909d0a94382059d07ae5c05e5c5daca1
Sakkachin Wongcharoen <rhythmsosad@gmail.com>
2015-11-14 13:48:38 +0700

    Create DB Model

---
+++ PDCA/PDCA/Controllers/QuestionaireController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PDCA.Controllers
+{
+    public class QuestionaireController : Controller
+    {
+        // GET: Questionaire
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
